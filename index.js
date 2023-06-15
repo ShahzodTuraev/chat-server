120a765d1fc74f916a593d504e283471210a4ac2
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-06-15 20:15:44 +0900

    server created

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io")
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on("connection", (socket)=>{
+  console.log(`User Connected: ${socket.id}`);
+  socket.on("join_room", ()=>{
+    socket.join()
+  })
+  socket.on("disconnect", ()=>{
+    console.log("User Disconnected", socket.id);
+  })
+})
+
+server.listen(4001, ()=>{
+  console.log('server run in port : 4001');
+});
+
+// https://www.youtube.com/watch?v=NU-HfZY3ATQ
